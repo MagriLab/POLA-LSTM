7909f5c80ba4beb1de1e6d6feca3e3c2f34ce29f
eliseoe <eliseoezalp@gmail.com>
2022-02-24 10:24:05 +0000

    changed for general window and batch size

--- lstm_model.py
+++ lstm_model.py
@@ -9,28 +9,51 @@ from data_processing import (
     create_training_split,
     df_training_split,
     create_df_3d,
-    create_window_closed_loop,
-    add_new_pred,
-    compute_lyapunov_time_arr,
     train_valid_test_split,
 )
 from loss import loss_oloop, loss_oloop_reg
 
 
-def select_random_window_with_label(df_transposed, n_windows):
-    idx = random.sample(range(len(df_transposed) - 50 - 1), n_windows)
+def create_window_closed_loop(test_data, iteration, window_size=50, pred=np.array([])):
+    if iteration == 0:
+        return test_data[:window_size, :].reshape(1, window_size, 3)
+    if iteration < window_size:
+        n_pred = pred.shape[0]
+        idx_test_entries = (
+            iteration + window_size - n_pred
+        )  # end index of entries from the test data,
+        test_data = test_data[iteration:idx_test_entries, :]
+        return np.append(test_data, pred, axis=0).reshape(1, window_size, 3)
+    else:
+        return pred[-window_size:, :].reshape(1, window_size, 3)
+
+
+def add_new_pred(pred_old, pred_new):
+    return np.append(pred_old, pred_new, axis=0)
+
+
+def compute_lyapunov_time_arr(time_vector, c_lyapunov=0.90566, window_size=50):
+    t_lyapunov = 1 / c_lyapunov
+    lyapunov_time = (time_vector[window_size:] - time_vector[window_size]) / t_lyapunov
+    return lyapunov_time
+
+
+def select_random_window_with_label(df_transposed, n_windows, window_size=50):
+    idx = random.sample(range(len(df_transposed) - window_size - 1), n_windows)
     # window_list =[]
-    window_list = [df_transposed[i : i + 50, :].reshape(1, 50, 3) for i in idx]
-    label_list = [df_transposed[i + 51, :].reshape(1, 3) for i in idx]
+    window_list = [
+        df_transposed[i : i + window_size, :].reshape(1, window_size, 3) for i in idx
+    ]
+    label_list = [df_transposed[i + window_size + 1, :].reshape(1, 3) for i in idx]
     return window_list, label_list, idx
 
 
-def select_random_batch_with_label(df_transposed):
-    idx_start = random.randint(0, len(df_transposed) - 50 - 1)
-    idx = np.arange(start=idx_start, stop=idx_start + 32)
+def select_random_batch_with_label(df_transposed, window_size=50, batch_size=32):
+    idx_start = random.randint(0, len(df_transposed) - window_size - 1)
+    idx = np.arange(start=idx_start, stop=idx_start + batch_size)
     # window_list =[]
-    window_list = [df_transposed[i : i + 50, :].reshape(1, 50, 3) for i in idx]
-    label_list = [df_transposed[i + 51, :].reshape(1, 3) for i in idx]
+    window_list = [df_transposed[i : i + window_size, :].reshape(1, 50, 3) for i in idx]
+    label_list = [df_transposed[i + window_size + 1, :].reshape(1, 3) for i in idx]
     return window_list, label_list, idx
 
 
@@ -41,32 +64,36 @@ def select_random_batches_with_label(
         df_transposed, n_int, window_size=window_size, batch_size=batch_size
     )
     idx_list = np.array(
-        [np.arange(start=idx_start, stop=idx_start + 32) for idx_start in idx]
+        [np.arange(start=idx_start, stop=idx_start + batch_size) for idx_start in idx]
     ).flatten()
     # for i in idx_list:
     #     print(i, len(df_transposed[i : i + 50, :]))
-    window_list = [df_transposed[i : i + 50, :].reshape(1, 50, 3) for i in idx_list]
-    label_list = [df_transposed[i + 51, :].reshape(1, 3) for i in idx_list]
+    window_list = [
+        df_transposed[i : i + window_size, :].reshape(1, window_size, 3)
+        for i in idx_list
+    ]
+    label_list = [df_transposed[i + window_size + 1, :].reshape(1, 3) for i in idx_list]
     return np.array(window_list), np.array(label_list), idx_list
 
 
 def random_int_window_dist(df_transposed, n_int, window_size=50, batch_size=32):
-    idx = [batch_size * i for i in sorted(random.sample(range(185), n_int))]
+    n_total_windows = int((len(df_transposed) - window_size) / batch_size)
+    idx = [batch_size * i for i in sorted(random.sample(range(n_total_windows)), n_int)]
     return idx
 
 
-def add_cloop_prediction(df_transposed, idx, predictions):
+def add_cloop_prediction(df_transposed, idx, predictions, window_size=50):
     window_list = np.array(
         [
             np.append(
-                df_transposed[idx[i] + 1 : idx[i] + 50, :],
+                df_transposed[idx[i] + 1 : idx[i] + window_size, :],
                 predictions[i, :].reshape(1, 3),
                 axis=0,
-            ).reshape(50, 3)
+            ).reshape(window_size, 3)
             for i in range(0, len(idx))
         ]
     )
-    label_list = np.array([df_transposed[i + 52, :] for i in idx])
+    label_list = np.array([df_transposed[i + window_size + 2, :] for i in idx])
     return window_list, label_list
 
 
