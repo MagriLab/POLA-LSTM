6f2fde61ddc0c63c858d090e5b9ac72553fe5851
eliseoe <eliseoezalp@gmail.com>
2022-03-24 11:45:57 +0000

    created function to transform custom losses into tensorboard events

--- lstm/postprocessing/tensorboard_converter.py
+++ lstm/postprocessing/tensorboard_converter.py
@@ -1,13 +1,12 @@
-import tensorflow as tf
-
-import numpy as np
-
 import os
 import time
 
+import numpy as np
+import tensorflow as tf
+
 
 def loss_arr_to_tensorboard(
-        logs_checkpoint, train_loss_dd_tracker, train_loss_pi_tracker, valid_loss_pi_tracker, valid_loss_dd_tracker):
+        logs_checkpoint, train_loss_dd_tracker, train_loss_pi_tracker, valid_loss_dd_tracker, valid_loss_pi_tracker):
     now = time.localtime()
     subdir = time.strftime("%d-%b-%Y_%H.%M.%S", now)
     summary_dir1 = os.path.join(logs_checkpoint, "train")
@@ -16,6 +15,7 @@ def loss_arr_to_tensorboard(
         with summary_writer1.as_default():
             tf.summary.scalar(name="epoch:physics_informed_loss", data=train_loss_pi_tracker[cont], step=cont)
             tf.summary.scalar(name="epoch:data_driven_loss", data=train_loss_dd_tracker[cont], step=cont)
+            tf.summary.scalar(name="epoch:total_loss", data=train_loss_dd_tracker[cont]+train_loss_pi_tracker[cont], step=cont)
         summary_writer1.flush()
 
     summary_dir1 = os.path.join(logs_checkpoint, "valid")
@@ -24,4 +24,5 @@ def loss_arr_to_tensorboard(
         with summary_writer1.as_default():
             tf.summary.scalar(name="epoch:physics_informed_loss", data=valid_loss_pi_tracker[cont], step=cont)
             tf.summary.scalar(name="epoch:data_driven_loss", data=valid_loss_dd_tracker[cont], step=cont)
+            tf.summary.scalar(name="epoch:total_loss", data=valid_loss_dd_tracker[cont]+valid_loss_pi_tracker[cont], step=cont)
     summary_writer1.flush()
