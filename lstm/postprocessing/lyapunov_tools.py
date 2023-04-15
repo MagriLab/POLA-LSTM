35a0b60c09f1b6f5f3b3fc23e94e3b52965c5356
Elise-Yael <eliseoezalp@gmail.com>
2023-04-15 13:43:31 +0100

    added kaplan yorke dimension

--- lstm/postprocessing/lyapunov_tools.py
+++ lstm/postprocessing/lyapunov_tools.py
@@ -139,3 +139,27 @@ def step_and_jac_analytical(u_t, h, c, model, args, idx, idx_lst=None, dim=None)
     Jac = tf.concat([tf.concat([Jac_c_new_c, Jac_c_new_h], axis=1),
                      tf.concat([Jac_h_new_c, Jac_h_new_h], axis=1)], axis=0)
     return Jac, u_t_temp, h_new, c_new
+
+
+
+def kaplan_yorke_dim(lyap_exponents):
+    """
+    Compute the Kaplan-Yorke dimension from an array of Lyapunov exponents.
+    
+    Parameters:
+    lyap_exponents (array): Array of Lyapunov exponents sorted in decreasing order.
+    
+    Returns:
+    kaplan_yorke_dim (float): The Kaplan-Yorke dimension of the system.
+    """
+    # Calculate the sum of positive Lyapunov exponents
+    sum_pos_lyap = np.sum(lyap_exponents[lyap_exponents > 0])
+    
+    # Find the index k of the largest Lyapunov exponent that satisfies the condition
+    # (sum of positive exponents)/(k+1) <= 0
+    k = np.argmax(np.cumsum(lyap_exponents)/(np.arange(len(lyap_exponents))+1) <= 0)
+    
+    # Compute the Kaplan-Yorke dimension as k + (sum of positive exponents)/(k+1)
+    kaplan_yorke_dim = k + sum_pos_lyap/(k+1)
+    
+    return kaplan_yorke_dim
